6d0eb4abb2feafbf362e732ceaff21e4f8ce6b10
FoushWare <Ahmed.fouad@kemetai.com>
2022-08-27 22:23:16 +0200

    [incidents,users] setup

---
+++ src/models/incident.model.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const { toJSON, paginate } = require('./plugins');
+
+const incidentsSchema = mongoose.Schema(
+  {
+    status: {
+      type: String,
+      required: true,
+      trim: true,
+      default: 'pending',
+    },
+    summary: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    reporter: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    assignee: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: 'User',
+    },
+    priority: {
+      type: Number,
+      required: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// add plugin that converts mongoose to json
+incidentsSchema.plugin(toJSON);
+incidentsSchema.plugin(paginate);
+
+/**
+ * @typedef User
+ */
+const User = mongoose.model('Incident', incidentsSchema);
+
+module.exports = User;
